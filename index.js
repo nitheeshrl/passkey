b0666cdad822d2edb576997221bf12ef72690388
nitheeshrl <rlnitheesh@gmail.com>
2024-11-13 23:00:22 +0530

    Update index.js11

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function timeout(){
 }
 timeout();
 var val=  cron.validate('* * * * *', timeout)
-var task = cron.schedule('*/10 * * * *', () =>  {
+var task = cron.schedule('* * * * * *', () =>  {
     console.log('will execute every minute until stopped');
     timeout();
   });
