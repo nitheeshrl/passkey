2d1b4b8ac94324cc6f36b56829564f39c9cc729e
nitheeshrl <rlnitheesh@gmail.com>
2024-11-14 01:30:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,12 +13,12 @@ console.log(`the db is connect with ${mongoose.connection.host}`)
 
 }
 conenctDB()
-var http = require('http');
-
-http.createServer(function (req, res) {
+var https = require('https');
+https.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-}).listen(8080);
+    res.write('Hello World!');
+    res.end();
+  }).listen(8080);
 function timeout(){
 console.log("Checking")
 }
