80781e02f25619c79e496ccd38ba83e0a23ef57f
nitheeshrl <rlnitheesh@gmail.com>
2024-11-16 11:21:19 +0530

    PASS HASH

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const crypto = require("node:crypto");
 const base64url = require('base64url');
 const mongoose = require ("mongoose")
 const Binary = require('binary')
-
+const bcrypt = require('bcrypt');
 const url = 'mongodb+srv://niftem:Niftem%40iac@niftem-t.lnsqf.mongodb.net/User-Passkey';
 const conenctDB = async()=>{
 await mongoose.connect (url)
@@ -144,6 +144,61 @@ var conn = mongoose.connection;
     // Login the user: Session, Cookies, JWT
     return res.json({ success: true, userId })
 })
-
+app.post("/hash", async (req, res) => {
+    const { password } = req.body;
+   console.log(password)
+      const start = Date.now();
+  
+      // genSalt
+      const salt = await bcrypt.genSalt(10)
+      console.log('salt: ' + salt);
+      console.log('salt cb end: ' + (Date.now() - start) + 'ms');
+  
+      // hash
+      const crypted = await bcrypt.hash(password, salt) 
+      console.log('crypted: ' + crypted);
+      console.log('crypted cb end: ' + (Date.now() - start) + 'ms');
+      console.log('rounds used from hash:', bcrypt.getRounds(crypted));
+  
+      // compare
+      const res11 = await bcrypt.compare(password, crypted)
+      console.log('compared true: ' + res);
+      console.log('compared true cb end: ' + (Date.now() - start) + 'ms');
+  
+  
+      console.log('end: ' + (Date.now() - start) + 'ms');
+      console.log(crypted)
+      console.log(crypted)
+      res.json({
+        hash: crypted,
+        salt: salt
+  
+  });
+   
+     
+  
+  
+  
+  })
+  app.post("/dehash", async (req, res) => {
+    const { password } = req.body;
+    const { salt } = req.body;
+   console.log(password)
+      const start = Date.now();
+      // hash
+      const crypted = await bcrypt.hash(password, salt) 
+      console.log('crypted: ' + crypted);
+      console.log('crypted cb end: ' + (Date.now() - start) + 'ms');
+      console.log('rounds used from hash:', bcrypt.getRounds(crypted));
+  
+      console.log('end: ' + (Date.now() - start) + 'ms');
+      console.log(crypted)
+      console.log(crypted)
+      res.json({
+        hash: crypted
+  
+  });
+   })
+    
 
 app.listen(PORT, () => console.log(`Server started on PORT:${PORT}`))
