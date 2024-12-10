708c3f1f9a2025bb7ce8ab7e963aed8ff491a94c
iacniftemt <rlnitheesh@gmail.com>
2024-12-10 18:19:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -230,7 +230,7 @@ username,
 
 app.get("/send-notification", async (req, res) => {
     var   { username, message } = (req.body);
-    username = "Anindit Bizy Nair";
+    username = "Nitheesh R L";
     message = "Hi from NIFTEM-T";
     var conn = mongoose.connection;
  var findresult = await conn.collection('Notifications').findOne({username: username});
